151b2110905ef47a79d6cb93effe9acd66aefe7e
yrimi1 <gofla8358@naver.com>
2021-11-10 16:50:12 +0900

    커밋커밋
    커밋

---
+++ my_js/my21.11.10/9999_memo.js
@@ -0,0 +1,13 @@
+//실험 용 페이지 입니다.
+
+
+let myarr1 = [1,11,111,1111]
+let myarr2 = [2,22,222,2222]
+console.log(myarr1)
+console.log(myarr2)
+
+for (let i in myarr1)
+    console.log(myarr1[i])
+
+for (let i in myarr2)
+    console.log(myarr2[i])
